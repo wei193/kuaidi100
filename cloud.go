b974f28614be1ed31d7b8b6ce9ce7676f36d32de
张魏征 <wei_193@qq.com>
2020-03-04 00:54:52 +0800

    增加打印

--- cloud.go
+++ cloud.go
@@ -55,6 +55,45 @@ type ManInfo struct {
 	Company   string `json:"company,omitempty"`
 }
 
+//ReqPrintOrder 云打印自定义请求
+type ReqPrintOrder struct {
+	Kuaidicom       string `json:"kuaidicom"`
+	Kuaidinum       string `json:"kuaidinum"`
+	RecManName      string `json:"recManName"`
+	RecManMobile    string `json:"recManMobile,omitempty"`
+	RecManTel       string `json:"recManTel,omitempty"`
+	RecManZipCode   string `json:"recManZipCode,omitempty"`
+	RecManProvince  string `json:"recManProvince,omitempty"`
+	RecManCity      string `json:"recManCity,omitempty"`
+	RecManDistrict  string `json:"recManDistrict,omitempty"`
+	RecManAddr      string `json:"recManAddr,omitempty"`
+	RecManPrintAddr string `json:"recManPrintAddr,omitempty"`
+	RecManCompany   string `json:"recManCompany,omitempty"`
+
+	SendManName      string `json:"sendManName"`
+	SendManMobile    string `json:"sendManMobile,omitempty"`
+	SendManTel       string `json:"sendManTel,omitempty"`
+	SendManZipCode   string `json:"sendManZipCode,omitempty"`
+	SendManProvince  string `json:"sendManProvince,omitempty"`
+	SendManCity      string `json:"sendManCity,omitempty"`
+	SendManDistrict  string `json:"sendManDistrict,omitempty"`
+	SendManAddr      string `json:"sendManAddr,omitempty"`
+	SendManPrintAddr string `json:"sendManPrintAddr,omitempty"`
+	SendManCompany   string `json:"sendManCompany,omitempty"`
+
+	Cargo       string  `json:"cargo,omitempty"`
+	Remark      string  `json:"remark,omitempty"`
+	Siid        string  `json:"siid"`
+	Tempid      string  `json:"tempid"`
+	Count       int     `json:"count"`
+	Weight      float32 `json:"weight"`
+	Volumn      float32 `json:"volumn"`
+	Height      int     `json:"height,omitempty"`
+	Width       int     `json:"width,omitempty"`
+	CallBackURL string  `json:"callBackUrl"`
+	Salt        string  `json:"salt,omitempty"`
+}
+
 //EOrder 快递100云打印电子面单接口 http://poll.kuaidi100.com/printapi/printtask.do?method=eOrder
 func (k *Kuaidi100) EOrder(data ReqeOrder) (res *ResError, err error) {
 
@@ -110,3 +149,34 @@ func (k *Kuaidi100) EOrder(data ReqeOrder) (res *ResError, err error) {
 // 	return
 
 // }
+
+//PrintOrder 快递100云打印自定义内容接口 http://poll.kuaidi100.com/printapi/printtask.do?method=printOrder
+func (k *Kuaidi100) PrintOrder(data ReqPrintOrder) (res *ResError, err error) {
+
+	buf, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err.Error() + "0")
+		return nil, err
+	}
+	t := fmt.Sprint(time.Now().Unix())
+	param := "method=printOrder"
+	param += "&key=" + k.Key
+	param += "&sign=" + Md5(string(buf)+t+k.Key+k.Secret)
+	param += "&t=" + fmt.Sprint(time.Now().Unix())
+	param += "&param=" + string(buf)
+	fmt.Println(param)
+	req, err := http.NewRequest("POST", "http://poll.kuaidi100.com/printapi/printtask.do?"+param, nil)
+	if err != nil {
+		fmt.Println(err.Error() + "1")
+		return nil, err
+	}
+	resBody, err := requset(req)
+	if err != nil {
+		fmt.Println(err.Error() + "2")
+		return nil, err
+	}
+	res = new(ResError)
+	err = json.Unmarshal(resBody, res)
+	return
+
+}
